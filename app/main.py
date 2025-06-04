43b64eb8f0a259fc43a7438c6c1bd0cbd76dd17a
moyu-king <1823931512@qq.com>
2025-06-05 00:09:38 +0800

    chore: 删除冗余代码

--- app/main.py
+++ app/main.py
@@ -1,24 +1,13 @@
 from fastapi import FastAPI
-from app.service.tencent import get_tencent_barrages, get_tencent_episodes
 from app.models.video import Video, db
-
 from app.service.tencent import tencent_router
 
 app = FastAPI()
 
 
-#路由分组
+# 路由分组
 app.include_router(tencent_router, prefix="/tencent", tags=["腾讯接口"])
 # app.include_router(bilibili.router, prefix="/bilibili", tags=["B战接口"])
-
-
-# @app.get("/episodes")
-# async def get_episodes():
-#     return await get_tencent_episodes()
-
-# @app.get("/barrages")
-# async def get_barrages():
-#     return await get_tencent_barrages()
 
 @app.get("/videos")
 async def get_videos():
--- app/service/tencent.py
+++ app/service/tencent.py
@@ -1,18 +1,17 @@
 from aiohttp import ClientSession
 from typing import Union
 from fastapi import APIRouter
- 
-tencent_router  = APIRouter()
+
+tencent_router = APIRouter()
 
 # https://dm.video.qq.com/barrage/segment/e0018sdzesg/t/v1/390000/420000
 # 获取弹幕
 @tencent_router.post("/barrage")
 async def tencent_barrage(time_offset1: str, time_offset2: str):
     async with ClientSession() as session:
-        
+
         response = await session.get("https://dm.video.qq.com/barrage/segment/e0018sdzesg/t/v1/" + time_offset1 + "/" + time_offset2)
         return barrage_response(await response.json())
-
 
 
 # 获取集数
@@ -43,7 +42,6 @@ async def tencent_episodes(cid: Union[str, None], vid: Union[str, None], pageCon
         return episode_response(await response.json(), pageContext)
 
 
-
 # 弹幕
 def barrage_response(response):
 
@@ -53,7 +51,7 @@ def barrage_response(response):
     }
     json_data['data'] = json_txt
     barrages = []
-    
+
     items = response['barrage_list']
     for item in items:
         barrage = {}
@@ -70,11 +68,11 @@ def episode_response(response, pageContext):
     json_data = {}
     json_txt = {}
     episodes = []
-    
+
     module_list_data = response['data']['module_list_datas'][0]
     item_datas = module_list_data['module_datas'][0]['item_data_lists']['item_datas']
     for item in item_datas:
-        
+
         item_params = item['item_params']
         if 'cid' in item_params:
             episode = {}
@@ -90,32 +88,7 @@ def episode_response(response, pageContext):
     return json_data
 
 
-
 async def get_tencent_barrages():
     async with ClientSession() as session:
         response = await session.get("https://dm.video.qq.com/barrage/segment/e0018sdzesg/t/v1/390000/420000")
-        return await response.json()
-
-
-async def get_tencent_episodes():
-    async with ClientSession() as session:
-        baseUrl = "https://pbaccess.video.qq.com/trpc.universal_backend_service.page_server_rpc.PageServer/GetPageData"
-        headers = {"referer": "https://v.qq.com"}
-        params = {"video_appid": "3000010", "vplatform": 2, "vversion_name": "8.2.96"}
-        payload = {
-            "page_params": {
-                "req_from": "web_vsite",
-                "page_id": "vsite_episode_list",
-                "page_type": "detail_operation",
-                "id_type": "1",
-                "cid": "mcv8hkc8zk8lnov",
-                "vid": "x0036x5qqsr",
-                "detail_page_type": "1",
-                "page_context": ""
-            },
-            "has_cache": 1
-        }
-
-        response = await session.post(baseUrl, headers=headers, params=params, json=payload)
-
         return await response.json()
