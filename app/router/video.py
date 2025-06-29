76d3900d4bf6067fe580d8a2f7214035086a75ec
moyu-king <1823931512@qq.com>
2025-06-29 16:12:44 +0800

    feat: 增加video创建api

--- app/router/video.py
+++ app/router/video.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
 from app.models.video import Video, db
 from app.models.resp import JsonResponse
+from pydantic import BaseModel
+
+class VideoCreateOption(BaseModel):
+    name: str
+    params: str
+    platform: int
+
 
 video_router = APIRouter()
 
@@ -11,3 +18,18 @@ async def get_videos():
             return JsonResponse.success(data=list(Video.select().dicts()))
     except Exception:
         return JsonResponse.fail(message="服务器内部错误")
+
+@video_router.post("/")
+async def create_video(opt: VideoCreateOption):
+    try:
+        with db.connection_context():
+            video = Video.create(
+                name=opt.name,
+                params=opt.params,
+                platform=opt.platform
+            )
+
+            return JsonResponse.success(data=video.__data__)
+    except Exception as e:
+        print(f"Video创建失败: {str(e)}")
+        JsonResponse.fail('创建失败')
